35b40f32a6596b0223eceaf08b0d2f23a00a1730
Dmitrii <fds16m576@student.bmstu.ru>
2024-07-02 21:50:34 +0300

    Реаизовано добавление и просмотр изображений через адм. панель

--- icecream/icecream_bar/admin.py
+++ icecream/icecream_bar/admin.py
@@ -2,23 +2,41 @@ from django.contrib import admin
 
 from django.contrib import admin
 from .models import Cup, IceCream, Topping, Order
+from django.utils.safestring import mark_safe
 
 
 # Класс администратора для модели Project
 @admin.register(Cup)
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
-    ordering = ('name',)
+    # Добвлять для изображения
+    readonly_fields = ('get_image',)
+    def get_image(self,obj):
+        return mark_safe(f'<img src={obj.image.url} width="80" height="100')
+
+    get_image.short_description = "Изображение"
 
 @admin.register(IceCream)
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     ordering = ('name',)
+    # Добвлять для изображения
+    readonly_fields = ('get_image',)
+    def get_image(self,obj):
+        return mark_safe(f'<img src={obj.image.url} width="80" height="100')
+
+    get_image.short_description = "Изображение"
 
 @admin.register(Topping)
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     ordering = ('name',)
+    # Добвлять для изображения
+    readonly_fields = ('get_image',)
+    def get_image(self,obj):
+        return mark_safe(f'<img src={obj.image.url} width="80" height="100')
+
+    get_image.short_description = "Изображение"
 
 # Класс администратора для модели Order
 @admin.register(Order)

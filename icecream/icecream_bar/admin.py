2c317c65f4eb7e7f4a791abf7ac71e81f6c8b976
Al63rt <khisamutdinovar@student.bmstu.ru>
2024-07-06 13:47:33 +0300

    Сделаны шаблоны страниц заказа и истории заказов.

--- icecream/icecream_bar/admin.py
+++ icecream/icecream_bar/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import (Flavor, Container, Topping, IceCreamInContainer)
+from .models import Flavor, Container, Topping, IceCreamInContainer
 from django.utils.safestring import mark_safe
 
 
